7af7f2aea3f292da0ceee86b1f4dbba1f80edbe5
tsunehito.takara <tsunehito.takara@l-is-b.com>
2016-04-15 15:12:51 +0900

    Add ignored field to simple test case

--- msgpack-bind-processor/src/test/resources/SimpleSource.java
+++ msgpack-bind-processor/src/test/resources/SimpleSource.java
@@ -12,6 +12,12 @@ public class SimpleSource {
     public Map<Long, String> fifth;
     public MimeTypes sixth;
     public byte[] seventh;
+    // private field is ignored.
+    private long eighth;
+    // static field is ignored.
+    static Long nineth;
+    // final field is ignored.
+    final Short tenth = 1;
 
     public enum MimeTypes {
         IMAGE, VIDEO, SOUND
